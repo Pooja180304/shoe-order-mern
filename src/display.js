692d7e45e835a68cd887fa926104569ff12985c3
Pooja180304 <spideypooja@gmail.com>
2023-11-29 19:38:25 +0530

    first commit

--- src/display.js
+++ src/display.js
@@ -1,38 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import axios from 'axios';
 
 const Display = () => {
   const [shoedata, shoedatachange] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/Details")
-      .then((res) => res.json())
-      .then((resp) => {
-        shoedatachange(resp);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+    axios.get('http://localhost:3001/display')
+    .then(shoedata=>shoedatachange(shoedata.data))
+    .catch(err=>console.log(err))
   }, []);
 
   const addTocart = (item) => {
-    const quantity = 1;
-    const newItem = { ...item, quantity };
-    fetch("http://localhost:3000/cart", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(newItem),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Item added to cart:", data);
-      toast.success("Item added to cart!");
-    })
-    .catch((error) => {
-      console.error("Error adding item to cart:", error);
-      toast.error("Error adding item to cart. Please try again later.");
-    });
+    const newItem = {
+        Shoename: item.Shoename,
+        Price: item.Price,
+        color: item.color,
+        Size: item.Size,
+        image: item.image,
+    };
+
+    axios.post('http://localhost:3001/addToCart', newItem)
+        .then((response) => {
+            console.log('Item added to cart:', response.data);
+            toast.success('Item added to cart!');
+        })
+        .catch((error) => {
+            console.error('Error adding item to cart:', error);
+            toast.error('Error adding item to cart. Please try again later.');
+        });
 };
 
   return (
@@ -47,8 +44,8 @@ const Display = () => {
         <p className="slogan">Step into Style and Comfort</p>
         </header>
         <div className="cont">
-	      {shoedata.map((item, index) => (
-        <div className="shoe-card">
+	      {shoedata.map(item => {
+        return <div className="shoe-card">
             <img className="img-resize" src={item.image} alt={item.Shoename}/>
             <h2>{item.Shoename}</h2>
             <p>Size: {item.size}</p>
@@ -56,7 +53,7 @@ const Display = () => {
             <p>${item.price}</p>
             <button type="submit" onClick={() => addTocart(item)}>Add to Cart</button>
           </div>
-      ))}
+      })}
       </div>
       </div>
   );
